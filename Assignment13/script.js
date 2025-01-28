efc22149873812e62dc5629ac6556f9a5e460458
Harish <harishvenkata65@gmail.com>
2025-01-28 21:51:24 +0530

    Assignment 13 Pushed

---
+++ Assignment13/script.js
@@ -0,0 +1,32 @@
+function checkCircle(moves) {
+  let x = 0,
+    y = 0;
+  for (let i = 0; i < moves.length; i++) {
+    switch (moves[i]) {
+      case "R":
+        x++;
+        break;
+      case "L":
+        x--;
+        break;
+      case "U":
+        y++;
+        break;
+      case "D":
+        y--;
+        break;
+    }
+  }
+  return x === 0 && y === 0;
+}
+
+let moves = prompt("Enter robot moves (R, L, U, D):");
+// document.write(checkCircle(moves));
+
+if (moves) {
+  moves = moves.toUpperCase();
+  let result = checkCircle(moves);
+  document.write(result);
+} else {
+  document.write("No moves entered.");
+}
