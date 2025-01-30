26407800886e16149f4101ee116c9179d51c2404
Harish <harishvenkata65@gmail.com>
2025-01-30 11:25:28 +0530

    Assignment 15 pushed

---
+++ Assignment15/script.js
@@ -0,0 +1,12 @@
+let num1 = +prompt("enter the first number");
+let num2 = +prompt("enter the second number");
+
+num1 = num1 + num2;
+num2 = num1 - num2;
+
+num1 = num1 - num2;
+
+console.log(num1);
+console.log(num2);
+
+// num1=3+4 =7  num2= 7-4=3 num1=7-3 =4
